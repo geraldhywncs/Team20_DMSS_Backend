9f9fa2c254e738a8f2a732506471aa29a447bd7b
geraldhywncs <gerald.hoo@ncs.com.sg>
2024-03-23 03:29:17 +0800

    finish delete friend api + test case

--- src/main/python/controller/friends_controller.py
+++ src/main/python/controller/friends_controller.py
@@ -14,3 +14,26 @@ class Friends_Controller:
             if not isinstance(friends, list):
                 return jsonify(message=friends), friends_status_code
             return jsonify(friends=friends), 200
+        
+        @app.route('/friends/<userID>', methods=["POST"])
+        def add_friend(userID):
+            data = request.get_json()
+
+            friends_db = Friends_Utility()
+            friend, friends_status_code = friends_db.create(user_id=userID, friend_id=data.get('friend_id'))
+            if not isinstance(friend, dict):
+                return jsonify(message=friend), friends_status_code
+            return jsonify(friend=friend), 200
+        
+        @app.route('/friends/<userID>', methods=["DELETE"])
+        def remove_friend(userID):
+            data = request.get_json()
+            print(data)
+
+            friends_db = Friends_Utility()
+            friend, friends_status_code = friends_db.delete(user_id=userID, friend_id=data.get('friend_id'))
+            print(friend)
+            print(friends_status_code)
+            if friends_status_code != 200:
+                return jsonify(message=friend), friends_status_code
+            return jsonify(message="Successfully removed friend"), 200
---
+++ src/main/python/tests/test_friends_create.py
@@ -0,0 +1,67 @@
+# update root dir
+import sys
+import os
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.dirname(current_dir)
+sys.path.append(parent_dir)
+
+# import
+from main import app
+from flask import json
+import pytest
+from config.database_config import db
+from utility.user_utility import User_Utility
+from utility.friends_utility import Friends_Utility
+
+@pytest.fixture
+def client():
+    with app.test_client() as client:
+        yield client
+
+@pytest.fixture(scope='function')
+def init_db():
+    """Initialize a clean database before each test."""
+    with app.app_context():
+        db.drop_all()
+        db.create_all()
+        yield db
+        db.session.rollback()
+
+@pytest.fixture(scope="function")
+def setup():
+    user_db = User_Utility()
+    user1, _ = user_db.create(
+        user_name="user1", 
+        email="user1@example.com", 
+        password="password", 
+        first_name="user", 
+        last_name="1"
+    )
+    user2, _ = user_db.create(
+        user_name="user2", 
+        email="user2@example.com", 
+        password="password", 
+        first_name="user", 
+        last_name="2"
+    )
+    yield (user1, user2)
+        
+def test_get_friends_success(client, init_db, setup):
+    user1, user2 = setup
+    headers = {'Content-Type': 'application/json'}
+    friends_db = Friends_Utility()
+
+    json_data = json.dumps({'friend_id': user2.get('user_id')})
+    response = client.post('/friends/' + str(user1.get('user_id')), data=json_data, headers=headers)
+    assert response.status_code == 200
+
+    data = json.loads(response.data)
+
+    friend = data['friend']
+    assert friend.get('user_id') == user1.get('user_id')
+    assert friend.get('friend_id') == user2.get('user_id')
+
+    friendList1, _ = friends_db.list_by_user_id(user_id=user1.get('user_id'))
+    assert len(friendList1) == 1
+    friendList2, _ = friends_db.list_by_user_id(user_id=user2.get('user_id'))
+    assert len(friendList2) == 0
