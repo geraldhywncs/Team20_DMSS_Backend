0f87f814d154528a487ced1f948136929e59e09d
DEVNET\d1358216 <weijie.fong@ncs.com.sg>
2024-04-06 01:00:36 +0800

    OCR implementation

--- src/main/python/controller/ocr_controller.py
+++ src/main/python/controller/ocr_controller.py
@@ -1,7 +1,8 @@
-# import cv2
-# import pytesseract
-# import numpy as np
-# from skimage.filters import threshold_local
+import cv2
+import pytesseract
+import numpy as np
+import re
+from skimage.filters import threshold_local
 from flask import request, jsonify
 from utility.ocr_utility import OCR_Utility
 
@@ -9,8 +10,10 @@ class OCR_Controller:
     def __init__(self, app):
         self.app = app
 
-        @app.route('/extract-text', methods=['POST'])
+
+        @app.route('/ocr', methods=['POST'])
         def run_ocr():
+            print('ocr called!')
             if 'image' not in request.files:
                 return jsonify({'error': 'No image provided'}), 400
 
@@ -18,18 +21,16 @@ class OCR_Controller:
 
             img = cv2.imdecode(np.frombuffer(image_file.read(), np.uint8), cv2.IMREAD_COLOR)
 
-            # Perform OCR
-            # extracted_text = self.perform_ocr(img)
             extracted_text = OCR_Utility.extract_text(img)
+            print(extracted_text)
 
-            # Clean output
-            # cleaned_text = self.clean_extracted(extracted_text)
+            cleaned_text = OCR_Utility.clean_text(extracted_text)
+            print(cleaned_text)
+            
+            if cleaned_text and re.match(r'^\d+(\.\d{1,2})?$', cleaned_text):
+                return jsonify(response =float(cleaned_text), ocr_status_code=200)
+            else:
+                return jsonify(response=cleaned_text,ocr_status_code=400), 400
 
-            # Return the extracted text
-            return jsonify({'text': extracted_text})
+            # return jsonify( receipt_amount = cleaned_text)
 
-
-    # def clean_extracted(self, img):
-    #     cleaned_text = ''
-
-    #     return jsonify({'text': extracted_text})
--- src/main/python/utility/ocr_utility.py
+++ src/main/python/utility/ocr_utility.py
@@ -1,23 +1,22 @@
-# import cv2
-# import pytesseract
-# import numpy
+import cv2
+import pytesseract
+import numpy
+from skimage.filters import threshold_local
+import re
 
-# from skimage.filters import threshold_local
-# from PIL import Image
-from flask import Flask, request, jsonify
 
 class OCR_Utility:
     # def __init__(self):
     #     self.ocr_utility = OCR_Utility()
     def extract_text(img):
         ###### Locate receipt in photo ######
-        
+
         # To resize if image too large
         def opencv_resize(image, ratio):
             width = int(image.shape[1] * ratio)
             height = int(image.shape[0] * ratio)
             dim = (width, height)
-            return cv2.resize(image, dim, interpolation = cv2.INTER_AREA)
+            return cv2.resize(image, dim, interpolation=cv2.INTER_AREA)
 
         ### Search for receipt in image ###
         # approximate the contour by a more primitive polygon shape
@@ -25,7 +24,7 @@ class OCR_Utility:
             peri = cv2.arcLength(contour, True)
             return cv2.approxPolyDP(contour, 0.032 * peri, True)
 
-        def get_receipt_contour(contours):    
+        def get_receipt_contour(contours):
             # loop over the contours
             for c in contours:
                 approx = approximate_contour(c)
@@ -33,15 +32,8 @@ class OCR_Utility:
                 if len(approx) == 4:
                     return approx
 
-
-        # Path to the image file
-        # image_path = 'Test_images\img4.jpg'
-        # image_path = request.files['image']
-        # image_path = img
-
         # Read the image using OpenCV
-
-        # image = cv2.imdecode(numpy.frombuffer(image_path.read(), numpy.uint8), cv2.IMREAD_COLOR)
+        print("ocr_utility called")
         image = img
 
         # Downscale image as finding receipt contour is more efficient on a small image
@@ -50,7 +42,6 @@ class OCR_Utility:
         image = opencv_resize(image, resize)
 
         # # Resize the image to a smaller size
-        # image = cv2.resize(image, (0, 0), fx=0.5, fy=0.5)
 
         # Convert the image to grayscale
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -64,39 +55,41 @@ class OCR_Utility:
 
         # Detect edges
         edged = cv2.Canny(dilated, 100, 200, apertureSize=3)
-        contours, hierarchy = cv2.findContours(edged, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-        image_with_contours = cv2.drawContours(image.copy(), contours, -1, (0,255,0), 3)
+        contours, hierarchy = cv2.findContours(
+            edged, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE
+        )
+        image_with_contours = cv2.drawContours(
+            image.copy(), contours, -1, (0, 255, 0), 3
+        )
 
-        largest_contours = sorted(contours, key = cv2.contourArea, reverse = True)[:10]
+        largest_contours = sorted(contours, key=cv2.contourArea, reverse=True)[:10]
 
         receipt_contour = get_receipt_contour(largest_contours)
-
 
         ###### Locate receipt in photo ######
 
         def contour_to_rect(contour):
             pts = contour.reshape(4, 2)
-            rect = numpy.zeros((4, 2), dtype = "float32")
+            rect = numpy.zeros((4, 2), dtype="float32")
             # top-left point has the smallest sum
             # bottom-right has the largest sum
-            s = pts.sum(axis = 1)
+            s = pts.sum(axis=1)
             rect[0] = pts[numpy.argmin(s)]
             rect[2] = pts[numpy.argmax(s)]
             # compute the difference between the points:
-            # the top-right will have the minumum difference 
+            # the top-right will have the minumum difference
             # the bottom-left will have the maximum difference
-            diff = numpy.diff(pts, axis = 1)
+            diff = numpy.diff(pts, axis=1)
             rect[1] = pts[numpy.argmin(diff)]
             rect[3] = pts[numpy.argmax(diff)]
             return rect / resize
-
 
         def wrap_perspective(img, rect):
             # unpack rectangle points: top left, top right, bottom right, bottom left
             (tl, tr, br, bl) = rect
             # compute the width of the new image
             widthA = numpy.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
-            
+
             widthB = numpy.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
             # compute the height of the new image
             heightA = numpy.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
@@ -106,24 +99,26 @@ class OCR_Utility:
             maxWidth = max(int(widthA), int(widthB))
             maxHeight = max(int(heightA), int(heightB))
             # destination points which will be used to map the screen to a "scanned" view
-            dst = numpy.array([
-                [0, 0],
-                [maxWidth - 1, 0],
-                [maxWidth - 1, maxHeight - 1],
-                [0, maxHeight - 1]], dtype = "float32")
+            dst = numpy.array(
+                [
+                    [0, 0],
+                    [maxWidth - 1, 0],
+                    [maxWidth - 1, maxHeight - 1],
+                    [0, maxHeight - 1],
+                ],
+                dtype="float32",
+            )
             # calculate the perspective transform matrix
             M = cv2.getPerspectiveTransform(rect, dst)
             # warp the perspective to grab the screen
             return cv2.warpPerspective(img, M, (maxWidth, maxHeight))
 
-
         def bw_scanner(image):
             gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-            T = threshold_local(gray, 21, offset = 10, method = "gaussian")
+            T = threshold_local(gray, 21, offset=10, method="gaussian")
             return (gray > T).astype("uint8") * 255
 
         scanned = wrap_perspective(original.copy(), contour_to_rect(receipt_contour))
-
 
         # Use pytesseract to extract text from the image
         result = bw_scanner(scanned)
@@ -131,20 +126,24 @@ class OCR_Utility:
 
         # For testing
 
-        print(outputText)
-        output = Image.fromarray(scanned)
-        output.save('result.png')
-
-        ### TO DO: Process the extracted text
-
-
+        print("detected output text: " + outputText)
 
         return outputText
 
-# if __name__ == '__main__':
-#     app.run(debug=True)
 
 
-    
-## curl -X POST -F "image=@testImage.jpg" http://127.0.0.1:5000/extract-text
-## curl -X POST -F "" http://127.0.0.1:5000/extract-text
+    def clean_text (extracted_text):
+        amount_patterns = [
+            r'(?:total|amount|due).*?([+-]?\d+(?:\.\d+)?)',
+            r'(?<=total|amount|due|balance).*?([+-]?\d+(?:\.\d+)?)',
+        # Add more patterns as needed
+        ]
+
+        final_amount = None
+        for pattern in amount_patterns:
+            match = re.search(pattern, extracted_text, re.IGNORECASE)
+            if match:
+                final_amount = match.group(1)
+                break
+
+        return final_amount
