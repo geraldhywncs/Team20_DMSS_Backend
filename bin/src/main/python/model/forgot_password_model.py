ba6ae1ee57cff519e8751ec371a70a13ff8d274d
Wee Jun Jie <d1357846@ncsdevnet.com>
2024-04-27 15:39:24 +0800

    update bandit security scanning pass and apply bandit security scanning to ci/cd pipiline

--- bin/src/main/python/model/forgot_password_model.py
+++
@@ -1,9 +0,0 @@
-from config.database_config import db
-
-class Reset_Password_Model(db.Model):
-    __bind_key__ = 'db1'
-    reset_password_id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer)
-    reset_token = db.Column(db.String(255))
-
-    __tablename__ = 'reset_password'
--- bin/src/main/python/model/icon_model.py
+++
@@ -1,8 +0,0 @@
-from config.database_config import db
-
-class Icon_Model(db.Model):
-    __bind_key__ = 'db1'
-    icon_id = db.Column(db.Integer, primary_key=True)
-    icon_name = db.Column(db.String(255))
-
-    __tablename__ = 'icon'
